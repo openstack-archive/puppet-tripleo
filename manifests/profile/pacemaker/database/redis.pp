fd1166a7c010bdca499f64874e1d46f8d4305eef
Pradeep Kilambi <pkilambi@redhat.com>
2016-04-12 12:01:19 -0400

    Composable roles within services - Redis
    Implements: blueprint refactor-puppet-manifests
    
    Co-Authored-By: Carlos Camacho <ccamacho@redhat.com>
    
    Change-Id: I60493a3aa64e5136b763e8e2084d728f5f812f8a

---
+++ manifests/profile/pacemaker/database/redis.pp
@@ -0,0 +1,63 @@
+# Copyright 2016 Red Hat, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+#
+# == Class: tripleo::profile::pacemaker::database::redis
+#
+# OpenStack Redis Pacemaker HA profile for tripleo
+#
+# === Parameters
+#
+# [*redis_vip*]
+#   Redis virtual IP
+#
+# [*bootstrap_node*]
+#   (Optional) The hostname of the node responsible for bootstrapping tasks
+#   Defaults to hiera('bootstrap_nodeid')
+#
+# [*enable_load_balancer*]
+#   (Optional) Whether load balancing is enabled for this cluster
+#   Defaults to hiera('enable_load_balancer', true)
+#
+# [*step*]
+#   (Optional) The current step in deployment. See tripleo-heat-templates
+#   for more details.
+#   Defaults to hiera('step')
+#
+
+class tripleo::profile::pacemaker::database::redis (
+  $redis_vip,
+  $bootstrap_node       = hiera('bootstrap_nodeid'),
+  $enable_load_balancer = hiera('enable_load_balancer', true),
+  $step                 = hiera('step'),
+) {
+  if $::hostname == downcase($bootstrap_node) {
+    $pacemaker_master = true
+  } else {
+    $pacemaker_master = false
+  }
+
+  if $step >= 1 {
+    include ::redis
+  }
+
+  if $step >= 2 and $pacemaker_master {
+    pacemaker::resource::ocf { 'redis':
+      ocf_agent_name  => 'heartbeat:redis',
+      master_params   => '',
+      meta_params     => 'notify=true ordered=true interleave=true',
+      resource_params => 'wait_last_known_master=true',
+      require         => Class['::redis'],
+    }
+  }
+}
