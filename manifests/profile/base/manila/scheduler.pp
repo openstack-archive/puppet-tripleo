2a80c93594cb554d91edf59896cdf93ad3721127
marios <marios@redhat.com>
2016-05-06 15:10:42 +0300

    Adds the puppet-tripleo manifests for manila
    The tripleo-heat-templates side that uses this is at
    https://review.openstack.org/#/c/188137/
    
    Change-Id: I444916d60a67bf730bf4089323dba1c1429e2e71
    Implements: blueprint refactor-puppet-manifests

---
+++ manifests/profile/base/manila/scheduler.pp
@@ -0,0 +1,46 @@
+# Copyright 2016 Red Hat, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+#
+# == Class: tripleo::profile::base::manila::scheduler
+#
+# Manila Scheduler profile for tripleo
+#
+# === Parameters
+#
+# [*step*]
+#   (Optional) The current step in deployment. See tripleo-heat-templates
+#   for more details.
+#   Defaults to hiera('step')
+#
+# [*sync_db*]
+#   (Optiona) Whether to run db sync.
+#   Defaults to true.
+#
+class tripleo::profile::base::manila::scheduler (
+  $step           = hiera('step'),
+  $sync_db        = true,
+) {
+
+  if $step >= 3 and $sync_db {
+    include ::manila::db::mysql
+  }
+
+  if $step >= 4 {
+    include ::manila::compute::nova
+    include ::manila::network::neutron
+    include ::manila::scheduler
+  }
+
+}
+
