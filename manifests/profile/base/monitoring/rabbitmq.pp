97e4aa5f761172477e0bccdcc762780ade71bf28
Martin Mágr <mmagr@redhat.com>
2016-05-23 13:15:42 +0200

    New composable service: Sensu
    Depends-On: Ic095cd1248cc2ffcef363893b20ff57f7befd6fc
    Change-Id: Ia09d14fa9db07595d2e904bd9918e5645ea590c7

---
+++ manifests/profile/base/monitoring/rabbitmq.pp
@@ -0,0 +1,54 @@
+# Copyright 2016 Red Hat, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+#
+# == Class: tripleo::profile::base::sensu::rabbitmq
+#
+# RabbitMQ configuration for Sensu stack for TripleO
+#
+# === Parameters
+#
+# [*vhost*]
+#   (Optional) String. RabbitMQ vhost to be used by Sensu
+#   Defaults to '/sensu'
+#
+# [*user*]
+#   (Optional) String. Username to connect to RabbitMQ server
+#   Defaults to hiera('rabbit_username', 'sensu')
+#
+# [*password*]
+#   (Optional) String. Password to connect to RabbitMQ server
+#   Defaults to hiera('rabbit_password', undef)
+#
+class tripleo::profile::base::monitoring::rabbitmq (
+  $vhost    = hiera('monitoring_rabbitmq_vhost', '/sensu'),
+  $user     = hiera('monitoring_rabbitmq_username', 'sensu'),
+  $password = hiera('monitoring_rabbitmq_password', undef),
+) {
+  rabbitmq_vhost { 'sensu-rabbit-vhost':
+    ensure => present,
+    name   => $vhost
+  }
+
+  rabbitmq_user { 'sensu-rabbit-user':
+    name     => $user,
+    password => $password,
+    tags     => ['monitoring']
+  }
+
+  rabbitmq_user_permissions { "${user}@${vhost}":
+    configure_permission => '.*',
+    read_permission      => '.*',
+    write_permission     => '.*',
+  }
+}
---
+++ manifests/profile/base/monitoring/sensu.pp
@@ -0,0 +1,36 @@
+# Copyright 2016 Red Hat, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+#
+# == Class: tripleo::profile::base::monitoring::sensu
+#
+# Sensu configuration for TripleO
+#
+# === Parameters
+#
+# [*step*]
+#   (Optional) String. The current step of the deployment
+#   Defaults to hiera('step')
+#
+class tripleo::profile::base::monitoring::sensu (
+  $step = hiera('step', undef),
+) {
+
+  if $step == undef or $step >= 3 {
+    include ::sensu
+
+    package { 'osops-tools-monitoring-oschecks':
+      ensure => 'present'
+    }
+  }
+}
