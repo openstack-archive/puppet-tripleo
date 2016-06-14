7c39de974cecb43849565e4b66722647531a468a
Emilien Macchi <emilien@redhat.com>
2016-06-14 19:20:25 -0400

    Create libvirt micro-service
    Create a new profile where we only manage Nova libvirt service.
    
    Change-Id: I2ff3cf6a31bc10aa46a089fcbfc654eda55e6ef8
    Implements: blueprint refactor-puppet-manifests
    Depends-On: Ib0d3111560af5af451e522c6dc3b3918d0463e7d

---
+++ manifests/profile/base/nova/libvirt.pp
@@ -0,0 +1,49 @@
+# Copyright 2016 Red Hat, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+#
+# == Class: tripleo::profile::base::nova::libvirt
+#
+# Libvirt profile for tripleo. It will deploy Libvirt service and configure it.
+#
+# === Parameters
+#
+# [*step*]
+#   (Optional) The current step in deployment. See tripleo-heat-templates
+#   for more details.
+#   Defaults to hiera('step')
+#
+class tripleo::profile::base::nova::libvirt (
+  $step = hiera('step'),
+) {
+
+  if $step >= 4 {
+    include ::tripleo::profile::base::nova
+    include ::nova::compute::libvirt::services
+
+    file { ['/etc/libvirt/qemu/networks/autostart/default.xml',
+      '/etc/libvirt/qemu/networks/default.xml']:
+      ensure => absent,
+      before => Service['libvirt'],
+    }
+
+    # in case libvirt has been already running before the Puppet run, make
+    # sure the default network is destroyed
+    exec { 'libvirt-default-net-destroy':
+      command => '/usr/bin/virsh net-destroy default',
+      onlyif  => '/usr/bin/virsh net-info default | /bin/grep -i "^active:\s*yes"',
+      before  => Service['libvirt'],
+    }
+  }
+
+}
