30d5be5f5179c645c5f594d8fd27fa8318f85cbe
Tomas Sedovic <tomas@sedovic.cz>
2016-09-12 17:32:48 +0200

    Add validations profile for tripleo
    This adds the tripleo::profile::base::validations profile which sets up
    the `validations` user and installs the openstack-tripleo-validations
    package.
    
    Change-Id: Ib2b1ddcda3a41cb7263171d3024f05ba8bfd2f28

---
+++ manifests/profile/base/validations.pp
@@ -0,0 +1,29 @@
+# Copyright 2016 Red Hat, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+#
+# == Class: tripleo::profile::base::validations
+#
+# Validations profile for tripleo
+#
+class tripleo::profile::base::validations {
+  package {'openstack-tripleo-validations': }
+  user {'validations':
+    ensure     => present,
+    comment    => 'TripleO validations',
+    managehome => true,
+    home       => '/home/validations',
+    shell      => '/sbin/nologin',
+  }
+}
+
