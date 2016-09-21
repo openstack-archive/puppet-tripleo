1c725dd46f37e2b21d5e1dece467e4f5567bd761
Alex Schultz <aschultz@redhat.com>
2016-09-21 15:21:49 -0600

    Add aodh profile rspec testing
    This change adds rspec testing for the aodh profile and serves as an
    example as to how to add in spec testing using hieradata to provide some
    required parameters.  This testing adds improved coverage for
    expectations around computed configuration items as well as for
    conditions around the steps within the tripleo deployment
    
    Change-Id: Ic763a544289a222fea97020a98821c1e375651a3

---
+++ spec/classes/tripleo_profile_base_aodh_notifier_spec.rb
@@ -0,0 +1,56 @@
+#
+# Copyright (C) 2016 Red Hat, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+#
+
+require 'spec_helper'
+
+describe 'tripleo::profile::base::aodh::notifier' do
+  shared_examples_for 'tripleo::profile::base::aodh::notifier' do
+    let(:pre_condition) do
+      "class { '::tripleo::profile::base::aodh': step => #{params[:step]}, rabbit_hosts => ['127.0.0.1'] }"
+    end
+
+    context 'with step less than 4' do
+      let(:params) { { :step => 3 } }
+
+      it 'should do nothing' do
+        is_expected.to contain_class('tripleo::profile::base::aodh::notifier')
+        is_expected.to contain_class('tripleo::profile::base::aodh')
+        is_expected.to_not contain_class('aodh::notifier')
+      end
+    end
+
+    context 'with step 4' do
+      let(:params) { {
+        :step => 4,
+      } }
+
+      it 'should trigger complete configuration' do
+        is_expected.to contain_class('aodh::notifier')
+      end
+    end
+  end
+
+
+  on_supported_os.each do |os, facts|
+    context "on #{os}" do
+      let(:facts) do
+        facts.merge({ :hostname => 'node.example.com' })
+      end
+
+      it_behaves_like 'tripleo::profile::base::aodh::notifier'
+    end
+  end
+end
