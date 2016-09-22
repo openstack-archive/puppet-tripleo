655bb235f522e7e11c18a4026d59b8a620ac9c1b
Emilien Macchi <emilien@redhat.com>
2016-09-22 11:11:13 -0400

    Switch puppet-tripleo to use puppet-openstack_spec_helper
    Align puppet-tripleo with other Puppet OpenStack modules to use
    puppet-openstack_spec_helper.
    
    Here are the benefits:
    
    - Allow to use Depends-On between Puppet OpenStack modules and
      puppet-tripleo, and unit tests will work correctly with dependencies.
    - Use the Puppet OpenStack gemspec file that handle gems dependencies to
      test the module.
    - Allow to re-use ruby helper that Puppet OpenStack modules have in unit
      tests, to avoid duplicated code.
    - Don't manage .fixtures.yml file, it will be generated by Puppet
      OpenStack tooling. TripleO dependencies will live in Puppetfile_extras.
    
    Change-Id: Ic66e9f872c57545327a9fb4b8ae86fbf0abbd8be
    (cherry picked from commit 1c4fe921e677c52dd01c9ce9aa12933fe0688b88)
    Depends-On: If24e073d0cb03ea8c4cc481c15870dc8dc53e9bc

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,19 @@
+# Load libraries from openstacklib here to simulate how they live together in a real puppet run (for provider unit tests)
+$LOAD_PATH.push(File.join(File.dirname(__FILE__), 'fixtures', 'modules', 'openstacklib', 'lib'))
 require 'puppetlabs_spec_helper/module_spec_helper'
 require 'shared_examples'
+
+require 'puppet-openstack_spec_helper/defaults'
+require 'rspec-puppet-facts'
+include RspecPuppetFacts
 
 fixture_path = File.expand_path(File.join(__FILE__, '..', 'fixtures'))
 
 RSpec.configure do |c|
   c.alias_it_should_behave_like_to :it_configures, 'configures'
   c.alias_it_should_behave_like_to :it_raises, 'raises'
+
+  c.hiera_config = File.join(fixture_path, 'hiera.yaml')
   c.module_path = File.join(fixture_path, 'modules')
   c.manifest_dir = File.join(fixture_path, 'manifests')
 
