9f9a6db9fc479355ee67ec45f170b77de56a1bdb
Juan Antonio Osorio Robles <jaosorior@redhat.com>
2016-09-27 08:58:50 +0000

    Enable TLS in the internal network for gnocchi
    This optionally enables TLS for gnocchi in the internal network.
    If internal TLS is enabled, each node that is serving the gnocchi
    service will use certmonger to request its certificate.
    
    bp tls-via-certmonger
    
    Change-Id: Ie983933e062ac6a7f0af4d88b32634e6ce17838b

--- manifests/profile/base/gnocchi/api.pp
+++ manifests/profile/base/gnocchi/api.pp
@@ -22,9 +22,38 @@
 #   (Optional) The hostname of the node responsible for bootstrapping tasks
 #   Defaults to hiera('bootstrap_nodeid')
 #
+# [*certificates_specs*]
+#   (Optional) The specifications to give to certmonger for the certificate(s)
+#   it will create.
+#   Example with hiera:
+#     apache_certificates_specs:
+#       httpd-internal_api:
+#         hostname: <overcloud controller fqdn>
+#         service_certificate: <service certificate path>
+#         service_key: <service key path>
+#         principal: "haproxy/<overcloud controller fqdn>"
+#   Defaults to hiera('apache_certificate_specs', {}).
+#
+# [*enable_internal_tls*]
+#   (Optional) Whether TLS in the internal network is enabled or not.
+#   Defaults to hiera('enable_internal_tls', false)
+#
+# [*generate_service_certificates*]
+#   (Optional) Whether or not certmonger will generate certificates for
+#   HAProxy. This could be as many as specified by the $certificates_specs
+#   variable.
+#   Note that this doesn't configure the certificates in haproxy, it merely
+#   creates the certificates.
+#   Defaults to hiera('generate_service_certificate', false).
+#
 # [*gnocchi_backend*]
 #   (Optional) Gnocchi backend string file, swift or rbd
 #   Defaults to swift
+#
+# [*gnocchi_network*]
+#   (Optional) The network name where the gnocchi endpoint is listening on.
+#   This is set by t-h-t.
+#   Defaults to hiera('gnocchi_api_network', undef)
 #
 # [*step*]
 #   (Optional) The current step in deployment. See tripleo-heat-templates
@@ -32,9 +61,13 @@
 #   Defaults to hiera('step')
 #
 class tripleo::profile::base::gnocchi::api (
-  $bootstrap_node  = hiera('bootstrap_nodeid', undef),
-  $gnocchi_backend = downcase(hiera('gnocchi_backend', 'swift')),
-  $step            = hiera('step'),
+  $bootstrap_node                = hiera('bootstrap_nodeid', undef),
+  $certificates_specs            = hiera('apache_certificates_specs', {}),
+  $enable_internal_tls           = hiera('enable_internal_tls', false),
+  $generate_service_certificates = hiera('generate_service_certificates', false),
+  $gnocchi_backend               = downcase(hiera('gnocchi_backend', 'swift')),
+  $gnocchi_network               = hiera('gnocchi_api_network', undef),
+  $step                          = hiera('step'),
 ) {
   if $::hostname == downcase($bootstrap_node) {
     $sync_db = true
@@ -44,13 +77,31 @@ class tripleo::profile::base::gnocchi::api (
 
   include ::tripleo::profile::base::gnocchi
 
+  if $enable_internal_tls {
+    if $generate_service_certificates {
+      ensure_resources('tripleo::certmonger::httpd', $certificates_specs)
+    }
+
+    if !$gnocchi_network {
+      fail('gnocchi_api_network is not set in the hieradata.')
+    }
+    $tls_certfile = $certificates_specs["httpd-${gnocchi_network}"]['service_certificate']
+    $tls_keyfile = $certificates_specs["httpd-${gnocchi_network}"]['service_key']
+  } else {
+    $tls_certfile = undef
+    $tls_keyfile = undef
+  }
+
   if $step >= 3 and $sync_db {
     include ::gnocchi::db::sync
   }
 
   if $step >= 4 {
     include ::gnocchi::api
-    include ::gnocchi::wsgi::apache
+    class { '::gnocchi::wsgi::apache':
+      ssl_cert => $tls_certfile,
+      ssl_key  => $tls_keyfile,
+    }
 
     class { '::gnocchi::storage':
       coordination_url => join(['redis://:', hiera('gnocchi_redis_password'), '@', normalize_ip_for_uri(hiera('redis_vip')), ':6379/']),
