858b220cc36a296efd49106f9358c70e38a29b21
Emilien Macchi <emilien@redhat.com>
2016-12-02 14:02:43 -0500

    Implement Nova Placement API profile
    Allow TripleO to deploy Nova Placement API with a new profile.
    
    Change-Id: I5e25a50f3d7a9b39f4146a61cb528963ee09e90c

--- manifests/profile/base/keystone.pp
+++ manifests/profile/base/keystone.pp
@@ -236,6 +236,9 @@ class tripleo::profile::base::keystone (
     if hiera('nova_api_enabled', false) {
       include ::nova::keystone::auth
     }
+    if hiera('nova_placement_enabled', false) {
+      include ::nova::keystone::auth_placement
+    }
     if hiera('panko_api_enabled', false) {
       include ::panko::keystone::auth
     }
