5fbe3853fbd1e4f22204f452156c2e78f73a5c1c
Dan Prince <dprince@redhat.com>
2017-02-20 13:53:57 -0500

    Ironic inspector support
    This includes a new ironic-inspector profile, and updates
    to the mysql and keystone profiles so that a database
    and endpoints are also created when the inspector
    is enabled.
    
    Change-Id: I4a71a95efb87a10528df0600277768969a32117b

---
+++ manifests/profile/base/ironic_inspector.pp
@@ -0,0 +1,46 @@
+# Copyright 2017 Red Hat, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+#
+# == Class: tripleo::profile::base::ironic_inspector
+#
+# Ironic inspector profile for TripleO
+#
+# === Parameters
+#
+# [*bootstrap_node*]
+#   (Optional) The hostname of the node responsible for bootstrapping tasks
+#   Defaults to hiera('bootstrap_nodeid')
+#
+# [*step*]
+#   (Optional) The current step of the deployment
+#   Defaults to hiera('step')
+
+class tripleo::profile::base::ironic_inspector (
+  $bootstrap_node = hiera('bootstrap_nodeid', undef),
+  $step           = hiera('step'),
+) {
+
+  if $::hostname == downcase($bootstrap_node) {
+    $sync_db = true
+  } else {
+    $sync_db = false
+  }
+
+  if $step >= 4 or ($step >= 3 and $sync_db) {
+    class { '::ironic::inspector':
+      sync_db => $sync_db,
+    }
+  }
+
+}
