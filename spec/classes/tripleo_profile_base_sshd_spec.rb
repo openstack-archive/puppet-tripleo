b35bc80ac2acf18463e4c18c8360862749aa0964
lhinds <lhinds@redhat.com>
2017-03-08 12:32:57 +0000

    SSHD Service extensions
    This change adds an `include` statement to bring in the extra
    functionality available from the existing puppet-ssh module in
    already available in RDO.
    
    By using puppet-ssh it provides a framework to allow the passing in of
    server options using just hiera values under ssh::server_options.
    For example, sshd_config banner can now be passed a server option, as
    well as all the new parameters outlined in the launchpad issue that
    the patch references for Closing. For this reason, the former augeas
    setting for `Banner /etc/issue` is now managed by the main puppet-ssh
    module instead.
    
    The change also allows population of MOTD text to `/etc/motd` as
    well as `issue.net`.
    
    $bannertext is refactored in accordance with patch [1]
    
    [1] https://review.openstack.org/#/c/442406/
    
    Change-Id: Id329538fb7b623526f1d91d8a513cf3440c86a7c
    Closes-Bug: 1668543

--- spec/classes/tripleo_profile_base_sshd_spec.rb
+++ spec/classes/tripleo_profile_base_sshd_spec.rb
@@ -1,4 +1,4 @@
-# Copyright 2016 Red Hat, Inc.
+# Copyright 2017 Red Hat, Inc.
 # All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License"); you may
@@ -13,18 +13,64 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 #
+# Unit tests for tripleo::profile::base::sshd
+#
 
 require 'spec_helper'
 
 describe 'tripleo::profile::base::sshd' do
 
-  context 'with banner configured' do
-    it do
-      is_expected.to contain_file('/etc/issue').with({
-        'owner'  => 'root',
-        'group'  => 'root',
-        'mode'   => '0600',
-        })
+  shared_examples_for 'tripleo::profile::base::sshd' do
+
+    context 'it should do nothing' do
+      it do
+        is_expected.to contain_class('ssh')
+        is_expected.to_not contain_file('/etc/issue')
+        is_expected.to_not contain_file('/etc/issue.net')
+        is_expected.to_not contain_file('/etc/motd')
+      end
+    end
+
+    context 'with issue and issue.net configured' do
+      let(:params) {{ :bannertext => 'foo' }}
+      it do
+        is_expected.to contain_file('/etc/issue').with({
+          'content' => 'foo',
+          'owner'   => 'root',
+          'group'   => 'root',
+          'mode'    => '0644',
+          })
+        is_expected.to contain_file('/etc/issue.net').with({
+          'content' => 'foo',
+          'owner'   => 'root',
+          'group'   => 'root',
+          'mode'    => '0644',
+          })
+        is_expected.to_not contain_file('/etc/motd')
+      end
+    end
+
+    context 'with motd configured' do
+      let(:params) {{ :motd => 'foo' }}
+      it do
+        is_expected.to contain_file('/etc/motd').with({
+          'content' => 'foo',
+          'owner'   => 'root',
+          'group'   => 'root',
+          'mode'    => '0644',
+          })
+        is_expected.to_not contain_file('/etc/issue')
+        is_expected.to_not contain_file('/etc/issue.net')
+      end
+    end
+  end
+
+  on_supported_os.each do |os, facts|
+    context "on #{os}" do
+      let (:facts) {
+        facts
+      }
+      it_behaves_like 'tripleo::profile::base::sshd'
     end
   end
 end
