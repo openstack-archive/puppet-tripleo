08b9633c50c757419ce80657ab59b5b3c0d261cc
Michael Henkel <mhenkel@juniper.net>
2017-03-22 15:08:36 +0100

    Adding listen_options for Contrail Webui https in haproxy
    Without balance source and hash-type consistent traffic to
    Contrail Webui https is not correctly load-balanced
    
    Change-Id: I05a5aeea7db801c1403ef3c4dd4f94480fd8692e

--- manifests/haproxy.pp
+++ manifests/haproxy.pp
@@ -1641,6 +1641,10 @@ class tripleo::haproxy (
       ip_addresses      => hiera('contrail_config_node_ips'),
       server_names      => hiera('contrail_config_node_ips'),
       public_ssl_port   => $ports[contrail_webui_https_port],
+      listen_options    => {
+          'balance'   => 'source',
+          'hash-type' => 'consistent',
+      }
     }
   }
 }
