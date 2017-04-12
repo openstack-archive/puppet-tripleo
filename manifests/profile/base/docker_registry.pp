5c8d5fd8ecd5c1a24df256950ad8c8524f6b6e01
Martin André <m.andre@redhat.com>
2017-04-12 18:06:15 +0200

    Make install of kolla optional on the undercloud
    This defaults to 'True' to keep backward compatibility and can be
    disabled by setting 'enable_container_images_built' to false in
    undercloud.conf.
    
    Depends-On: Ia3379cf66b1d6b180def69c2a5b22b2602baacef
    Change-Id: I33e7e9a6a3865fed38f7ed6490455457da67782b

--- manifests/profile/base/docker_registry.pp
+++ manifests/profile/base/docker_registry.pp
@@ -31,10 +31,15 @@
 #  network
 #  Defaults to hiera('controller_admin_host')
 #
+# [*enable_container_images_build*]
+#  (Optional) Whether to install tools to build docker container images
+#  Defaults to hiera('enable_container_images_build', true)
+#
 class tripleo::profile::base::docker_registry (
-  $registry_host       = hiera('controller_host'),
-  $registry_port       = 8787,
-  $registry_admin_host = hiera('controller_admin_host'),
+  $registry_host                 = hiera('controller_host'),
+  $registry_port                 = 8787,
+  $registry_admin_host           = hiera('controller_admin_host'),
+  $enable_container_images_build = hiera('enable_container_images_build', true),
 ) {
 
   include ::tripleo::profile::base::docker
@@ -45,7 +50,9 @@ class tripleo::profile::base::docker_registry (
     allow_virtual => false,
   }
   package{'docker-distribution': }
-  package{'openstack-kolla': }
+  if str2bool($enable_container_images_build) {
+    package{'openstack-kolla': }
+  }
   file { '/etc/docker-distribution/registry/config.yml' :
     ensure  => file,
     content => template('tripleo/docker_distribution/registry_config.yml.erb'),
