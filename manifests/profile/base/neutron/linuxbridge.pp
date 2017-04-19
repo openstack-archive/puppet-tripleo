f8ed8b69ddd56b43675a7ecdfb8e61c207ad44b1
Bartosz Stopa <b.stopa@samsung.com>
2017-04-19 09:18:27 +0200

    Add linuxbridge agent profile
    Add a tripleo profile for neutron linuxbridge agent configuration.
    
    Change-Id: Ie3ac03052f341c26735b423701e1decf7233d935
    Partial-Bug: #1652211

---
+++ manifests/profile/base/neutron/linuxbridge.pp
@@ -0,0 +1,20 @@
+# == Class: tripleo::profile::base::neutron::linuxbridge
+#
+# Neutron linuxbridge agent profile for tripleo
+#
+# === Parameters
+#
+# [*step*]
+#   (Optional) The current step in deployment. See tripleo-heat-templatee
+#   for more details.
+#   Defaults to hiera('step')
+#
+class tripleo::profile::base::neutron::linuxbridge(
+    $step           = hiera('step'),
+) {
+    include ::tripleo::profile::base::neutron
+
+    if $step >= 5 {
+        include ::neutron::agents::ml2::linuxbridge
+    }
+}
