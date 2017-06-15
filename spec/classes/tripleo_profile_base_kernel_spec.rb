0566aaef1be00e9dbd7b3fad4028b9c5eb7bf24b
Or Idgar <oidgar@redhat.com>
2017-06-15 13:41:02 +0000

    Ignore failures when loading nf_conntrack_proto_sctp kernel module
    Ignore failures if nf_conntrack_proto_sctp module failed to load.
    Since RHEL 7.4, nf_conntrack_proto_sctp module is compiled into the
    kernel instead of as a module as the sctp support.
    TripleO will still try to load the module to support RHEL 7.3, but
    in the future will remove the module management and rely on the kernel
    provided in newer versions of RHEL.
    
    Co-Authored-By: Or Idgar <oidgar@redhat.com>
    Co-Authored-By: Alex Schultz <aschultz@redhat.com>
    Co-Authored-By: Emilien Macchi <emilien@redhat.com>
    
    Change-Id: I8f1c841a7c0f3b1247aba2b959b6dfbe43d8cd79
    Closes-Bug: 1695885
    (cherry picked from commit 76eb1bbd4f977e16c97516500f050f8b49e7399d)

---
+++ spec/classes/tripleo_profile_base_kernel_spec.rb
@@ -0,0 +1,59 @@
+#
+# Copyright (C) 2017 Red Hat, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+#
+
+require 'spec_helper'
+
+describe 'tripleo::profile::base::kernel' do
+
+    shared_examples_for 'tripleo::profile::base::kernel' do
+        context 'with kernel modules' do
+           let :params do
+               {
+                   :module_list => {
+                     'nf_conntrack' => {},
+                   }
+               }
+           end
+
+            it 'should load kernel module' do
+                is_expected.to contain_kmod__load('nf_conntrack')
+            end
+        end
+        context 'with sysctl settings' do
+           let :params do
+               {
+                   :sysctl_settings => {
+                     'net.ipv4.tcp_keepalive_intvl' => { 'value' => '1'},
+                   }
+               }
+           end
+
+            it 'should load kernel module' do
+                is_expected.to contain_sysctl__value('net.ipv4.tcp_keepalive_intvl')
+            end
+        end
+    end
+
+    on_supported_os.each do |os, facts|
+        context "on #{os}" do
+            let(:facts) {
+                facts
+            }
+
+        it_behaves_like 'tripleo::profile::base::kernel'
+        end
+    end
+end
