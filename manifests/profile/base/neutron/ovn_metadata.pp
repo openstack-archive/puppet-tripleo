f2abe0c4c8403184011328cc6be1da63b6e9844a
Daniel Alvarez <dalvarez@redhat.com>
2017-09-12 12:08:36 +0200

    Add support for OVN Metadata Agent
    This patch adds support for configuring networking-ovn-metadata-agent.
    
    Depends-On: I38f775479d178f5b252619635b67f876bc8c5ed5
    
    Change-Id: Ifdd42437333730a3b3e6f36cbab6df0a2971a5a1
    Signed-off-by: Daniel Alvarez <dalvarez@redhat.com>

---
+++ manifests/profile/base/neutron/ovn_metadata.pp
@@ -0,0 +1,45 @@
+# Copyright 2017 Red Hat, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+#
+# == Class: tripleo::profile::base::neutron::ovn_metadata
+#
+# Networking-ovn Metadata Agent profile for tripleo
+#
+# === Parameters
+#
+# [*ovn_db_host*]
+#   The IP-Address where OVN DBs are listening.
+#   Defaults to hiera('ovn_dbs_vip')
+#
+# [*ovn_sb_port*]
+#   (Optional) Port number on which southbound database is listening
+#   Defaults to hiera('ovn::southbound::port')
+#
+# [*step*]
+#   (Optional) The current step in deployment. See tripleo-heat-templates
+#   for more details.
+#   Defaults to hiera('step')
+#
+class tripleo::profile::base::neutron::ovn_metadata (
+  $ovn_db_host = hiera('ovn_dbs_vip'),
+  $ovn_sb_port = hiera('ovn::southbound::port'),
+  $step = Integer(hiera('step')),
+) {
+  if $step >= 4 {
+    class { '::neutron::agents::ovn_metadata':
+      ovn_sb_connection => "tcp:${ovn_db_host}:${ovn_sb_port}",
+    }
+    Service<| title == 'controller' |> -> Service<| title == 'ovn-metadata' |>
+  }
+}
