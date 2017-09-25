e5c563290cbd0c12056169403043aef41246e353
waleed mousa <waleedm@mellanox.com>
2017-09-25 09:31:20 -0400

    Add support for switchdev mode in SR-IOV
    In Kernel 4.10 supports changing SR-IOV to switchdev mode.
    This mode allows to create VFs represontors which can manage
    the SR-IOV VFs from the hypervsior.
    
    This patch extends the tripleo::host::sriov::number_of_vf to
    <physical_network>:<number_of_vfs>:<sriov_mode>,
    where sriov_mode accepts legacy or switchdev.
    if sriov_mode is not specified we default to legacy.
    
    Change-Id: I578f956f2a8c6ee29a9d1ff38ee51765bcab05c1

--- spec/unit/provider/sriov_vf_config/numvfs_spec.rb
+++ spec/unit/provider/sriov_vf_config/numvfs_spec.rb
@@ -35,6 +35,83 @@ describe provider_class do
     it 'should return path of the file to enable vfs' do
       expect(provider.sriov_numvfs_path).to eql('/sys/class/net/eth0/device/sriov_numvfs')
     end
+    it 'should return ovs mode: legacy' do
+      expect(provider.ovs_mode).to eql('legacy')
+    end
+  end
+
+  let :numvfs_conf_switchdev do
+    {
+      :name   => 'eth1:12:switchdev',
+      :ensure => 'present',
+    }
+  end
+
+  describe 'when setting the attributes' do
+    let :resource_switchdev do
+      Puppet::Type::Sriov_vf_config.new(numvfs_conf_switchdev)
+    end
+
+    let :provider_switchdev do
+      provider_class.new(resource_switchdev)
+    end
+
+    it 'should return the correct interface name' do
+      expect(provider_switchdev.sriov_get_interface).to eql('eth1')
+    end
+
+    it 'should return the correct numvfs value' do
+      expect(provider_switchdev.sriov_numvfs_value).to eql(12)
+    end
+
+    it 'should return path of the file to enable vfs' do
+      expect(provider_switchdev.sriov_numvfs_path).to eql('/sys/class/net/eth1/device/sriov_numvfs')
+    end
+
+    it 'should return path of the vendor file' do
+      expect(provider_switchdev.vendor_path).to eql('/sys/class/net/eth1/device/vendor')
+    end
+
+    it 'should return ovs mode: switchdev' do
+      expect(provider_switchdev.ovs_mode).to eql('switchdev')
+    end
+  end
+
+  let :numvfs_conf_legacy do
+    {
+      :name   => 'eth2:14:legacy',
+      :ensure => 'present',
+    }
+  end
+
+  describe 'when setting the attributes' do
+    let :resource_legacy do
+      Puppet::Type::Sriov_vf_config.new(numvfs_conf_legacy)
+    end
+
+    let :provider_legacy do
+      provider_class.new(resource_legacy)
+    end
+
+    it 'should return the correct interface name' do
+      expect(provider_legacy.sriov_get_interface).to eql('eth2')
+    end
+
+    it 'should return the correct numvfs value' do
+      expect(provider_legacy.sriov_numvfs_value).to eql(14)
+    end
+
+    it 'should return path of the file to enable vfs' do
+      expect(provider_legacy.sriov_numvfs_path).to eql('/sys/class/net/eth2/device/sriov_numvfs')
+    end
+
+    it 'should return path of the vendor file' do
+      expect(provider_legacy.vendor_path).to eql('/sys/class/net/eth2/device/vendor')
+    end
+
+    it 'should return ovs mode: legacy' do
+      expect(provider_legacy.ovs_mode).to eql('legacy')
+    end
   end
 
 end
