7f537d384080d51bfc902fa0ec62b1326b941247
Rajesh Tailor <ratailor@redhat.com>
2017-09-28 11:10:22 +0530

    Add TLS for ec2api service
    This adds a TLS proxy in front of it so it serves TLS in the
    internal network.
    
    bp tls-via-certmonger
    Change-Id: I24d990eccf7affd5f3899338ac96d02d2d47460e
    (cherry picked from commit 24a3e20eeda2efad0a6d61927ab0848bc8b625a8)

--- manifests/profile/base/nova/ec2api.pp
+++ manifests/profile/base/nova/ec2api.pp
@@ -18,15 +18,84 @@
 #
 # === Parameters
 #
+# [*bootstrap_node*]
+#   (Optional) The hostname of the node responsible for bootstrapping tasks
+#   Defaults to hiera('bootstrap_nodeid')
+#
+# [*certificates_specs*]
+#   (Optional) The specifications to give to certmonger for the certificate(s)
+#   it will create.
+#   Example with hiera:
+#     apache_certificates_specs:
+#       httpd-internal_api:
+#         hostname: <overcloud controller fqdn>
+#         service_certificate: <service certificate path>
+#         service_key: <service key path>
+#         principal: "haproxy/<overcloud controller fqdn>"
+#   Defaults to hiera('apache_certificate_specs', {}).
+#
+# [*enable_internal_tls*]
+#   (Optional) Whether TLS in the internal network is enabled or not.
+#   Defaults to hiera('enable_internal_tls', false)
+#
+# [*ec2_api_network*]
+#   (Optional) The network name where the ec2api endpoint is listening on.
+#   This is set by t-h-t.
+#   Defaults to hiera('ec2_api_network', undef)
+#
 # [*step*]
 #   (Optional) The current step in deployment. See tripleo-heat-templates
 #   for more details.
 #   Defaults to hiera('step')
 #
+# [*ec2_api_tls_proxy_bind_ip*]
+#   IP on which the TLS proxy will listen on. Required only used if
+#   enable_internal_tls is set.
+#   Defaults to undef
+#
+# [*ec2_api_tls_proxy_fqdn*]
+#   fqdn on which the tls proxy will listen on. Required only used if
+#   enable_internal_tls is set.
+#   Defaults to undef
+#
+# [*ec2_api_tls_proxy_port*]
+#   port on which the tls proxy will listen on. Only used if
+#   enable_internal_tls is set.
+#   Defaults to 8788
+#
 class tripleo::profile::base::nova::ec2api (
-  $step    = Integer(hiera('step'))
+  $bootstrap_node                = hiera('bootstrap_nodeid', undef),
+  $certificates_specs            = hiera('apache_certificates_specs', {}),
+  $enable_internal_tls           = hiera('enable_internal_tls', false),
+  $ec2_api_network               = hiera('ec2_api_network', undef),
+  $step                          = Integer(hiera('step')),
+  $ec2_api_tls_proxy_bind_ip     = undef,
+  $ec2_api_tls_proxy_fqdn        = undef,
+  $ec2_api_tls_proxy_port        = 8788,
 ) {
-  if $step >= 4 {
+  if $::hostname == downcase($bootstrap_node) {
+    $sync_db = true
+  } else {
+    $sync_db = false
+  }
+
+  if $step >= 4 or ($step >= 3 and $sync_db) {
+    if $enable_internal_tls {
+      if !$ec2_api_network {
+        fail('ec2_api_network is not set in the hieradata.')
+      }
+      $ec2_api_tls_certfile = $certificates_specs["httpd-${ec2_api_network}"]['service_certificate']
+      $ec2_api_tls_keyfile = $certificates_specs["httpd-${ec2_api_network}"]['service_key']
+
+      ::tripleo::tls_proxy { 'ec2-api':
+        servername => $ec2_api_tls_proxy_fqdn,
+        ip         => $ec2_api_tls_proxy_bind_ip,
+        port       => $ec2_api_tls_proxy_port,
+        tls_cert   => $ec2_api_tls_certfile,
+        tls_key    => $ec2_api_tls_keyfile,
+      }
+      Tripleo::Tls_proxy['ec2-api'] ~> Anchor<| title == 'ec2api::service::begin' |>
+    }
     include ::ec2api
     include ::ec2api::api
     include ::ec2api::db::sync
