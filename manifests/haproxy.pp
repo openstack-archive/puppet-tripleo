b8456e5cee400c207800637cf6cf0833708566a2
Sven Anderson <sven@redhat.com>
2017-11-03 17:47:55 +0100

    Change haproxy check to tcpka for ec2_api
    EC2-API always returns HTTP code 400 if not properly authorized,
    therefore httpchk is not working, since it's expecting an 200 OK.
    Changing to tcpka for now, until EC2-API implements a URL that is
    suitable for healthchecks.
    
    Change-Id: Ic8ec8cdd4dc59e3768c06912ceb8a91b425b0a06

--- manifests/haproxy.pp
+++ manifests/haproxy.pp
@@ -1076,6 +1076,9 @@ class tripleo::haproxy (
       server_names      => hiera('ec2_api_node_names', $controller_hosts_names_real),
       mode              => 'http',
       public_ssl_port   => $ports[ec2_api_ssl_port],
+      listen_options    => merge($default_listen_options, {
+        'option' => [ 'tcpka' ]
+      }),
       service_network   => $ec2_api_network,
     }
   }
