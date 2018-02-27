7fb7d63c2b26a4705faba8fcf3c9ea593c460cdc
Alex Schultz <aschultz@redhat.com>
2018-02-27 13:01:45 -0700

    Move ip_forward configuration to THT
    The ip_forward configuration is now handled in the kernel configuration
    of THT. We need to drop the static configuration of this
    net.ipv4.ip_forward value.
    
    Depends-On: I557e4a41c4e5be3a2f50e5d5ddc86e17c1eb44e1
    Change-Id: I9e97ef74bbbe5287692fb4b5cb8e229e3f975741

--- manifests/profile/base/neutron/plumgrid.pp
+++ manifests/profile/base/neutron/plumgrid.pp
@@ -27,10 +27,6 @@ class tripleo::profile::base::neutron::plumgrid (
 
   if $step >= 4 {
 
-    # forward all ipv4 traffic
-    # this is required for the vms to pass through the gateways public interface
-    sysctl::value { 'net.ipv4.ip_forward': value => '1' }
-
     # ifc_ctl_pp needs to be invoked by root as part of the vif.py when a VM is powered on
     file { '/etc/sudoers.d/ifc_ctl_sudoers':
       ensure  => file,
