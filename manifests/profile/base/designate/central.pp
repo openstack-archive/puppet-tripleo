ee78d70c37e4b7204c02ee20c6f9bf1929d89560
Thomas Herve <therve@redhat.com>
2018-11-09 14:30:31 +0100

    Fix designate db instantation
    designate::central now requires designate::db
    (I696745e2d57aade6cbb0fe89e334fddf1beaafdb), so we need to instantiate
    the latter first.
    
    Change-Id: I15cdc27468f6e54dc14c8ffd818f326cb93ba2e3

--- manifests/profile/base/designate/central.pp
+++ manifests/profile/base/designate/central.pp
@@ -53,11 +53,11 @@ class tripleo::profile::base::designate::central (
   }
   include ::tripleo::profile::base::designate
   if ($step >= 4 or ($step >= 3 and $sync_db)) {
-    include ::designate::central
-    include ::designate::quota
     class { '::designate::db':
       sync_db => $sync_db,
     }
+    include ::designate::central
+    include ::designate::quota
   }
   if ($step == 5 and $sync_db) {
     exec { 'pool update':
