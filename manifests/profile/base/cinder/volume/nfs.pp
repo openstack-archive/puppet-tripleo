8851b8836bcd9ceca69d7398ecec427a50e75203
Alan Bishop <abishop@redhat.com>
2018-12-06 11:29:15 -0500

    Add ability to configure Cinder's NFS snapshot support
    Add the ability to configure the nfs_snapshot_support parameter
    associated with Cinder's NFS backend.
    
    Closes-bug: #1823101
    Change-Id: I4df8e3941eb074339e399e5a5c44fa411ff21560
    (cherry picked from commit I4df8e3941eb074339e399e5a5c44fa411ff21560)

--- manifests/profile/base/cinder/volume/nfs.pp
+++ manifests/profile/base/cinder/volume/nfs.pp
@@ -29,6 +29,10 @@
 #   (Optional) List of mount options for the NFS share
 #   Defaults to ''
 #
+# [*cinder_nfs_snapshot_support*]
+#   (Optional) Whether to enable support for snapshots in the NFS driver.
+#   Defaults to $::os_service_default
+#
 # [*cinder_nas_secure_file_operations*]
 #   (Optional) Allow network-attached storage systems to operate in a secure
 #   environment where root level access is not permitted. If set to False,
@@ -55,6 +59,7 @@ class tripleo::profile::base::cinder::volume::nfs (
   $cinder_nfs_servers,
   $backend_name                       = hiera('cinder::backend::nfs::volume_backend_name', 'tripleo_nfs'),
   $cinder_nfs_mount_options           = '',
+  $cinder_nfs_snapshot_support        = $::os_service_default,
   $cinder_nas_secure_file_operations  = $::os_service_default,
   $cinder_nas_secure_file_permissions = $::os_service_default,
   $step                               = Integer(hiera('step')),
@@ -67,6 +72,7 @@ class tripleo::profile::base::cinder::volume::nfs (
       nfs_servers                 => $cinder_nfs_servers,
       nfs_mount_options           => $cinder_nfs_mount_options,
       nfs_shares_config           => '/etc/cinder/shares-nfs.conf',
+      nfs_snapshot_support        => $cinder_nfs_snapshot_support,
       nas_secure_file_operations  => $cinder_nas_secure_file_operations,
       nas_secure_file_permissions => $cinder_nas_secure_file_permissions,
     }
