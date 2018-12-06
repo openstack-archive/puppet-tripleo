bd9846062c22be898d8720d1ee4ffbb65808fc8f
Juan Antonio Osorio Robles <jaosorior@redhat.com>
2018-12-06 18:46:31 -0500

    Reload HAProxy when certificate is renewed
    This is meant to fix the issue of the certificate renewal not
    automatically restarting/reloading the haproxy service.
    
    It's all done by a script that's installed by puppet.
    
    Preferably this patch and the one pointed by this should merge at the
    same time.
    
    Co-Authored-By: Grzegorz Grasza <xek@redhat.com>
    Needed-By: Id409899bf04e7f9f2653e6c48cfebd0a92ca2d08
    Change-Id: I5d91f8d9b5cd4f86ae0511a69e58858c5dccd35d

---
+++ files/certmonger-haproxy-refresh.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+# This script is meant to reload HAProxy when certmonger triggers a certificate
+# renewal. It'll concatenate the needed certificates for the PEM file that
+# HAProxy reads.
+
+die() { echo "$*" 1>&2 ; exit 1; }
+
+[[ $# -eq 2 ]] || die "Invalid number of arguments"
+[[ $1 == @(reload|restart) ]] || die "First argument must be one of 'reload' or 'restart'."
+
+
+ACTION=$1
+NETWORK=$2
+
+certmonger_ca=$(hiera -c /etc/puppet/hiera.yaml certmonger_ca)
+container_cli=$(hiera -c /etc/puppet/hiera.yaml container_cli docker)
+service_certificate="$(hiera -c /etc/puppet/hiera.yaml tripleo::certmonger::haproxy_dirs::certificate_dir)/overcloud-haproxy-$NETWORK.crt"
+service_key="$(hiera -c /etc/puppet/hiera.yaml tripleo::certmonger::haproxy_dirs::key_dir)/overcloud-haproxy-$NETWORK.key"
+ca_path=""
+
+if [ "$certmonger_ca" == "local" ]; then
+    ca_path="/etc/pki/ca-trust/source/anchors/cm-local-ca.pem"
+elif [ "$certmonger_ca" == "IPA" ]; then
+    ca_path="/etc/ipa/ca.crt"
+fi
+
+if [ "$NETWORK" != "external" ]; then
+    service_pem="$(hiera -c /etc/puppet/hiera.yaml tripleo::certmonger::haproxy_dirs::certificate_dir)/overcloud-haproxy-$NETWORK.pem"
+else
+    service_pem="$(hiera -c /etc/puppet/hiera.yaml tripleo::haproxy::service_certificate)"
+fi
+
+cat "$service_certificate" "$ca_path" "$service_key" > "$service_pem"
+
+haproxy_container_name=$($container_cli ps --format="{{.Names}}" | grep haproxy)
+
+if [ "$ACTION" == "reload" ]; then
+    # Copy the new cert from the mount-point to the real path
+    $container_cli exec "$haproxy_container_name" cp "/var/lib/kolla/config_files/src-tls$service_pem" "$service_pem"
+
+    # Set appropriate permissions
+    $container_cli exec "$haproxy_container_name" chown haproxy:haproxy "$service_pem"
+
+    # Trigger a reload for HAProxy to read the new certificates
+    pkill -f -HUP haproxy-systemd-wrapper
+elif [ "$ACTION" == "restart" ]; then
+    # Copying the certificate and permissions will be handled by kolla's start
+    # script.
+    $container_cli restart "$haproxy_container_name"
+fi
