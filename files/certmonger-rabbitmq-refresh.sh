801391a13eec513f7e0f5dba09b1214e4db8abf4
Grzegorz Grasza <xek@redhat.com>
2019-01-25 14:54:00 +0100

    rabbitmq:  Remove default post-save command for certmonger
    The default command didn't work, so we need to fix that.
    
    The script additionally copies the certificates in the right place
    and instead of restarting RabbitMQ, it triggers a pem cache reload.
    
    Related-Bug: #1811401
    Needed-By: I3e564f9a5abdbf11d0580c4ff801092f32bcc678
    Change-Id: Id06633a1adaafe1fef1d3d7f6b2af3ef5ffc9d4a

---
+++ files/certmonger-rabbitmq-refresh.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+container_cli=$(hiera -c /etc/puppet/hiera.yaml container_cli docker)
+
+container_name=$($container_cli ps --format="{{.Names}}" | grep rabbitmq)
+
+service_pem="$(hiera -c /etc/puppet/hiera.yaml tripleo::rabbitmq::service_certificate)"
+
+# Copy the new cert from the mount-point to the real path
+$container_cli exec "$container_name" cp "/var/lib/kolla/config_files/src-tls$service_pem" "$service_pem"
+
+# Set appropriate permissions
+$container_cli exec "$container_name" chown rabbitmq:rabbitmq "$service_pem"
+
+# Trigger a pem cache clear in RabbitMQ to read the new certificates
+$container_cli exec $container_name rabbitmqctl eval "ssl:clear_pem_cache()."
