b1bc0410dea380857874e2ed4d6d527fea25ca01
Juan Antonio Osorio Robles <jaosorior@redhat.com>
2019-03-20 15:26:56 +0200

    Enable internal TLS options for ironic API
    This was missing from the haproxy configuration. This commit sets that
    up.
    
    Change-Id: I33a16d79cc26c8c239b575361517e0151f14503c
    Related-Bug: #1820815
    (cherry picked from commit e38e6d5d4f191252a2aeb28fe76b2d326af73e49)

--- manifests/haproxy.pp
+++ manifests/haproxy.pp
@@ -1304,6 +1304,7 @@ class tripleo::haproxy (
       mode              => 'http',
       public_ssl_port   => $ports[ironic_api_ssl_port],
       service_network   => $ironic_network,
+      member_options    => union($haproxy_member_options, $internal_tls_member_options),
     }
   }
 
