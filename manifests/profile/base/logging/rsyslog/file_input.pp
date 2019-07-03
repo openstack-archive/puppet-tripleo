f8eb9096de435ef36af1e4747bbaa4cc3096c72b
Martin Magr <mmagr@redhat.com>
2019-07-03 09:30:58 +0200

    Remove unnecessary logic
    THT logic which exposed fluentd logging hieradata even when the service
    was not enabled was removed, so the logic in rsyslog file_input working
    with such data won't ever work.
    
    Change-Id: I58edc44f859a0b46c1b1ae82c453df9f4d2bc487

--- manifests/profile/base/logging/rsyslog/file_input.pp
+++ manifests/profile/base/logging/rsyslog/file_input.pp
@@ -16,28 +16,13 @@ define tripleo::profile::base::logging::rsyslog::file_input (
   $sources = hiera("tripleo_logging_sources_${title}", undef),
   $default_startmsg = '^[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2}(.[0-9]+ [0-9]+)? (DEBUG|INFO|WARNING|ERROR) '
 ) {
-  # TODO(mmagr): As a deprecation path for fluentd we gonna use the fluentd THT parameters first.
-  # We can remove below few lines after fluentd is removed and it's template outputs
-  # are renamed to provider-agnostic name in all composable services
-  if !$sources {
-    $fluentd_sources = hiera("tripleo_fluentd_sources_${title}", [])
-    $rsyslog_sources = $fluentd_sources.reduce([]) |$memo, $config| {
-      if $config['startmsg.regex'] {
-        $startmsg = $config['startmsg.regex']
-      } else {
-        $startmsg = $default_startmsg
-      }
-      $memo + [{'file' => $config['path'], 'tag' => $config['tag'], 'startmsg.regex' => $startmsg }]
+  $rsyslog_sources = $sources.reduce([]) |$memo, $config| {
+    if ! $config['startmsg.regex'] {
+      $record = $config + {'startmsg.regex' => $default_startmsg}
+    } else {
+      $record = $config
     }
-  } else {
-    $rsyslog_sources = $sources.reduce([]) |$memo, $config| {
-      if ! $config['startmsg.regex'] {
-        $record = $config + {'startmsg.regex' => $default_startmsg}
-      } else {
-        $record = $config
-      }
-      $memo + [$record]
-    }
+    $memo + [$record]
   }
 
   $rsyslog_sources.each |$config| {
