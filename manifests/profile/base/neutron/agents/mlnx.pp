969467960552400a4cd555be1db4d24f1e2013a2
Hamdy Khader <hamdyk@mellanox.com>
2019-08-06 11:53:24 +0300

    Add base class for neutron Mellanox agent
    Change-Id: Ibd817a4a34ee2944ee86b4a47e7621637d258da3

---
+++ manifests/profile/base/neutron/agents/mlnx.pp
@@ -0,0 +1,34 @@
+#
+# == Class: tripleo::profile::base::neutron::agents::mlnx
+#
+# Neutron Mellanox Agent profile for tripleo
+#
+# === Parameters
+#
+# [*step*]
+#   (Optional) The current step in deployment. See tripleo-heat-templates
+#   for more details.
+#   Defaults to hiera('step')
+#
+
+class tripleo::profile::base::neutron::agents::mlnx(
+  $step = Integer(hiera('step'))
+) {
+
+  file { '/etc/neutron/plugins/mlnx':
+    ensure => directory,
+  }
+
+  file { '/etc/neutron/plugins/mlnx/mlnx_conf.ini':
+    ensure  => file,
+    owner   => 'root',
+    group   => 'neutron',
+    require => File['/etc/neutron/plugins/mlnx'],
+    mode    => '0640',
+  }
+
+  if $step >= 3 {
+
+    include ::neutron::agents::ml2::mlnx
+  }
+}
