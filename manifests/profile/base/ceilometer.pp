7edb80bb7161a685330e32c2d7becd37c124ef12
Chandan kumar <chkumar@redhat.com>
2019-08-21 15:50:04 +0530

    Remove deprecated ceilometer::dispatcher::gnocchi
    https://review.opendev.org/#/c/677427/ removes the same from
    puppet-ceilometer so it also needs to be removed puppet-tripleo
    
    Closes-Bug: #1840901
    
    Change-Id: If4ab08cdcf43c14c37ac35f9044d363b09833d0c
    Signed-off-by: Chandan kumar <chkumar@redhat.com>

--- manifests/profile/base/ceilometer.pp
+++ manifests/profile/base/ceilometer.pp
@@ -154,7 +154,6 @@ class tripleo::profile::base::ceilometer (
       }
     }
     include ::ceilometer::config
-    include ::ceilometer::dispatcher::gnocchi
     include ::ceilometer::logging
   }
 }
