165ed10dc1d7f5cd09eaab838d7651b11e12b6c3
Francesco Pantano <fpantano@redhat.com>
2020-03-09 14:46:06 +0100

    Fix restart unit condition on radosgw
    This change just fixes the restart condition
    for the radosgw file used when the certificate
    is renewed.
    
    Change-Id: Id3f76cd03c993d013090c7c764d6963a64a1c74f

--- files/certmonger-rgw-refresh.sh
+++ files/certmonger-rgw-refresh.sh
@@ -4,6 +4,6 @@
 rgw_unit=$(systemctl list-unit-files | awk '/radosgw/ {print $1}')
 
 # Restart the rgw systemd unit
-if [ -z "$rgw_unit" ]; then
+if [ -n "$rgw_unit" ]; then
     systemctl restart "$rgw_unit"
 fi
