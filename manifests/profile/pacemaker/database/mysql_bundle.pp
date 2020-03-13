57de7a597b47a44258153598a3190eb913066c71
Luca Miccini <lmiccini@redhat.com>
2020-03-13 11:43:34 +0100

    Allow override of mysql/galera open files limit
    This commit removes the hard coded value of open-file-limit (16384)
    and allows operators to override it via:
    
    tripleo::profile::pacemaker::database::mysql::open_files_limit
    tripleo::profile::pacemaker::database::mysql_bundle::open_files_limit
    
    Change-Id: I4927eb65a2dc1b5a86fc2141c7b2713f36ad49de
    Resolves: rhbz#1812969

--- manifests/profile/pacemaker/database/mysql_bundle.pp
+++ manifests/profile/pacemaker/database/mysql_bundle.pp
@@ -136,6 +136,10 @@
 #   (optional) Set the --user= switch to be passed to pcmk
 #   Defaults to 'root'
 #
+# [*open_files_limit*]
+#   (Optional) Maximum value for open-files-limit
+#   Defaults to 16384
+#
 class tripleo::profile::pacemaker::database::mysql_bundle (
   $mysql_docker_image             = hiera('tripleo::profile::pacemaker::database::mysql_bundle::mysql_docker_image', undef),
   $control_port                   = hiera('tripleo::profile::pacemaker::database::mysql_bundle::control_port', '3123'),
@@ -161,6 +165,7 @@ class tripleo::profile::pacemaker::database::mysql_bundle (
   $bundle_user                    = 'root',
   $pcs_tries                      = hiera('pcs_tries', 20),
   $step                           = Integer(hiera('step')),
+  $open_files_limit               = 16384,
 ) {
   if $::hostname == downcase($bootstrap_node) {
     $pacemaker_master = true
@@ -464,7 +469,7 @@ MYSQL_HOST=localhost\n",
         master_params   => '',
         meta_params     => "master-max=${galera_nodes_count} ordered=true container-attribute-target=host",
         op_params       => 'promote timeout=300s on-fail=block',
-        resource_params => "log='/var/log/mysql/mysqld.log' additional_parameters='--open-files-limit=16384' enable_creation=true wsrep_cluster_address='gcomm://${galera_nodes}' cluster_host_map='${cluster_host_map_string}'",
+        resource_params => "log='/var/log/mysql/mysqld.log' additional_parameters='--open-files-limit=${open_files_limit}' enable_creation=true wsrep_cluster_address='gcomm://${galera_nodes}' cluster_host_map='${cluster_host_map_string}'",
         tries           => $pcs_tries,
         location_rule   => {
           resource_discovery => 'exclusive',
