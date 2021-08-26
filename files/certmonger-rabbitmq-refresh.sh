48d6566567234e7269a29eff8d56208dd49b2884
Damien Ciabrini <dciabrin@redhat.com>
2021-08-26 12:53:24 +0200

    Fix rabbitmq certificate reload after it is resubmitted
    When certmonger resubmit a certificate, make sure that
    the post_save command reads the right hiera key to
    update the certificate file into the running rabbitmq
    container.
    
    Change-Id: Ic7f66b83611794d41105941c15c32479fe876980
    Closes-Bug: #1941727
    (cherry picked from commit 42a29d04139f938f180b66060d8580325a57ff11)

--- files/certmonger-rabbitmq-refresh.sh
+++ files/certmonger-rabbitmq-refresh.sh
@@ -5,8 +5,8 @@ container_cli=$(hiera -c /etc/puppet/hiera.yaml container_cli docker)
 
 container_name=$($container_cli ps --format="{{.Names}}" | grep -w -E 'rabbitmq(-bundle-.*-[0-9]+)?')
 
-service_crt="$(hiera -c /etc/puppet/hiera.yaml tripleo::rabbitmq::service_certificate.service_certificate)"
-service_key="$(hiera -c /etc/puppet/hiera.yaml tripleo::rabbitmq::service_certificate.service_key)"
+service_crt="$(hiera -c /etc/puppet/hiera.yaml tripleo::profile::base::rabbitmq::certificate_specs.service_certificate)"
+service_key="$(hiera -c /etc/puppet/hiera.yaml tripleo::profile::base::rabbitmq::certificate_specs.service_key)"
 
 if echo "$container_name" | grep -q "^rabbitmq-bundle"; then
   # lp#1917868: Do not use podman cp with HA containers as they get
@@ -24,9 +24,6 @@ else
   # Copy the new key from the mount-point to the real path
   $container_cli exec -u root "$container_name" cp "/var/lib/kolla/config_files/src-tls$service_key" "$service_key"
 fi
-
-# Copy the new cert from the mount-point to the real path
-$container_cli exec "$container_name" cp "/var/lib/kolla/config_files/src-tls$service_pem" "$service_pem"
 
 # Set appropriate permissions
 $container_cli exec -u root "$container_name" chown rabbitmq:rabbitmq "$service_crt"
