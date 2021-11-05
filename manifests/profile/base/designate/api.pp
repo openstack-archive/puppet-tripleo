1bc9d4279b94f7298024cc41b7a299ce9f8637a6
Brent Eagles <beagles@redhat.com>
2021-11-05 16:32:33 -0230

    Run designate-api under apache
    Designate should be run under apache for production deployments.
    
    Depends-on: https://review.opendev.org/c/openstack/puppet-designate/+/823704
    Change-Id: I3b566d3da33ff583e47ce241f02da19ec889bcc3
    (cherry picked from commit d674f63e9119a57d3d33f8ad6b9db08ad8e36f39)

--- manifests/profile/base/designate/api.pp
+++ manifests/profile/base/designate/api.pp
@@ -23,27 +23,75 @@
 #   for more details.
 #   Defaults to hiera('step')
 #
+# [*certificates_specs*]
+#   (Optional) The specifications to give to certmonger for the certificate(s)
+#   it will create.
+#   Example with hiera:
+#     apache_certificates_specs:
+#       httpd-internal_api:
+#         hostname: <overcloud controller fqdn>
+#         service_certificate: <service certificate path>
+#         service_key: <service key path>
+#         principal: "haproxy/<overcloud controller fqdn>"
+#   Defaults to hiera('apache_certificate_specs', {}).
+#
+# [*enable_internal_tls*]
+#   (Optional) Whether TLS in the internal network is enabled or not.
+#   Defaults to hiera('enable_internal_tls', false)
+#
+# [*designate_network*]
+#   (Optional) The network name where the designate endpoint is listening on.
+#   This is set by t-h-t.
+#   Defaults to hiera('designate_api_network', undef)
+#
+# DEPRECATED PARAMETERS
+#
 # [*listen_ip*]
-#   (Optional) The IP on which the API should listen.
-#   Defaults to 0.0.0.0
+#   (Optional) The IP on which the API should listen.  (now set by hiera via
+#   designate::wsgi::apache)
+#   Defaults to undef
 #
 # [*listen_port*]
-#   (Optional) The port on which the API should listen.
-#   Defaults to 9001
+#   (Optional) The port on which the API should listen. (no longer needed,
+#   listen port gets default value from designate::wsgi::apache)
+#   Defaults to undef
 #
 class tripleo::profile::base::designate::api (
-  $step           = Integer(hiera('step')),
-  $listen_ip      = '0.0.0.0',
-  $listen_port    = '9001',
+  $step                = Integer(hiera('step')),
+  $certificates_specs  = hiera('apache_certificates_specs', {}),
+  $enable_internal_tls = hiera('enable_internal_tls', false),
+  $designate_network   = hiera('designate_api_network', undef),
+  $listen_ip           = undef,
+  $listen_port         = undef
 ) {
-
   include tripleo::profile::base::designate
   include tripleo::profile::base::designate::authtoken
 
+  if $enable_internal_tls {
+    if !$designate_network {
+      fail('designate_api_network is not set in the hieradata.')
+    }
+    $tls_certfile = $certificates_specs["httpd-${designate_network}"]['service_certificate']
+    $tls_keyfile = $certificates_specs["httpd-${designate_network}"]['service_key']
+  } else {
+    $tls_certfile = undef
+    $tls_keyfile = undef
+  }
+
   if ($step >= 3) {
-    $listen_uri = normalize_ip_for_uri($listen_ip)
-    class { 'designate::api':
-      listen => "${listen_uri}:${listen_port}",
+    # TODO: remove once the tripleo heat template changes merge
+    if $listen_ip and $listen_port {
+      $listen_uri = normalize_ip_for_uri($listen_ip)
+      class { 'designate::api':
+        listen => "${listen_uri}:${listen_port}"
+      }
+    } else {
+      include tripleo::profile::base::apache
+      class { 'designate::wsgi::apache':
+        ssl_cert => $tls_certfile,
+        ssl_key  => $tls_keyfile
+      }
+      include designate::api
     }
     include designate::healthcheck
   }
