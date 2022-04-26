4c2449055cbd4fae18f1136be9cdd61274b50894
Takashi Kajinami <tkajinam@redhat.com>
2022-04-26 16:22:18 +0900

    Logging: Replace hiera by lookup
    The hiera function is deprecated and does not work with the latest
    hieradata version 5. It should be replaced by the new lookup
    function[1].
    
    [1] https://puppet.com/docs/puppet/7/hiera_automatic.html
    
    With the lookup function, we can define value type and merge behavior,
    but these are kept default at this moment to limit scope of this change
    to just simple replacement. Adding value type might be useful to make
    sure the value is in expected type (especially when a boolean value is
    expected), but we will revisit that later.
    
    example:
    lookup(<NAME>, [<VALUE TYPE>], [<MERGE BEHAVIOR>], [<DEFAULT VALUE>])
    
    Change-Id: I9ddceedcf1b7d5335beea542e671de8f9f143e5f

--- manifests/profile/base/logging/rsyslog/file_input.pp
+++ manifests/profile/base/logging/rsyslog/file_input.pp
@@ -3,7 +3,7 @@
 #
 # [*sources*]
 #   (Optional) List of hashes. Contains configuration of file inputs for given service.
-#   Defaults to hiera("tripleo_logging_sources_${title}", undef)
+#   Defaults to lookup("tripleo_logging_sources_${title}", undef, undef, undef)
 #
 # [*default_startmsg*]
 #   (Optional) String. Default POSIX ERE for start of log record. The default enables to forward
@@ -18,7 +18,7 @@
 #   Defaults to undef
 #
 define tripleo::profile::base::logging::rsyslog::file_input (
-  $sources = hiera("tripleo_logging_sources_${title}", undef),
+  $sources = lookup("tripleo_logging_sources_${title}", undef, undef, undef),
   $default_startmsg = '^[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2}(.[0-9]+ [0-9]+)? (DEBUG|INFO|WARNING|ERROR) ',
   Optional[Enum['on','off']] $reopen_on_truncate = undef
 ) {
