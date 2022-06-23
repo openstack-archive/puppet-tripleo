openstack-archive/puppet-tripleo

commit 7b3a8cd8919399e0309c6343894a594e528d2f9a

Include db classes explicitly
Since database parameters in the base classes were deprecated, it is
likely that db classes are no longer included automatically in a future
release. Let's ensure that the db classes are included so that
database parameters are always set.

Change-Id: I2a28cd1b7a92776b711eb784db3c4a486dcf6a85

manifests/profile/base/octavia.pp
@@ -68,6 +68,7 @@ class tripleo::profile::base::octavia (
         }),
     }
     include octavia::config
+    include octavia::db
     include octavia::logging
     include octavia::service_auth
   }

spec/classes/tripleo_profile_base_octavia_spec.rb
@@ -34,6 +34,7 @@ describe 'tripleo::profile::base::octavia' do
       it 'should not do anything' do
         is_expected.to_not contain_class('octavia')
         is_expected.to_not contain_class('octavia::config')
+        is_expected.to_not contain_class('octavia::db')
       end
     end
 
@@ -47,6 +48,7 @@ describe 'tripleo::profile::base::octavia' do
           :default_transport_url => 'rabbit://guest:password@some.server.com:5672/?ssl=0'
         )
         is_expected.to contain_class('octavia::config')
+        is_expected.to contain_class('octavia::db')
       end
     end
 

manifests/profile/base/aodh.pp
@@ -121,6 +121,7 @@ class tripleo::profile::base::aodh (
     }
     include aodh::service_credentials
     include aodh::config
+    include aodh::db
     include aodh::db::sync
     include aodh::logging
   }

manifests/profile/base/barbican.pp
@@ -31,5 +31,6 @@ class tripleo::profile::base::barbican (
   if $step >= 3 {
     include barbican
     include barbican::config
+    include barbican::db
   }
 }

manifests/profile/base/ceilometer.pp
@@ -112,6 +112,7 @@ class tripleo::profile::base::ceilometer (
     }
 
     include ceilometer::config
+    include ceilometer::db
     include ceilometer::logging
   }
 }

manifests/profile/base/cinder.pp
@@ -123,6 +123,7 @@ class tripleo::profile::base::cinder (
       }),
     }
     include cinder::config
+    include cinder::db
     include cinder::glance
     include cinder::nova
     include cinder::logging

manifests/profile/base/glance/api.pp
@@ -211,6 +211,7 @@ class tripleo::profile::base::glance::api (
     include glance
     include glance::config
     include glance::healthcheck
+    include glance::api::db
     include glance::api::logging
     class { 'glance::api':
       enabled_backends => $enabled_backends,

manifests/profile/base/gnocchi.pp
@@ -91,9 +91,9 @@ class tripleo::profile::base::gnocchi (
     class { 'gnocchi':
       coordination_url => join(['redis://:', $gnocchi_redis_password, '@', normalize_ip_for_uri($redis_vip), ':6379/', $tls_query_param]),
     }
-    include gnocchi::db
     include gnocchi::config
     include gnocchi::cors
+    include gnocchi::db
     include gnocchi::logging
   }
 }

commit c5db68dc0dbbc784646ae8c15ae7dad695d233eb

Octavia: Add optional inclusion of driver_agent class
... so that we can maintain all of class inclusions in puppet-tripleo.

Conflicts:
	spec/classes/tripleo_profile_base_octavia_spec.rb

Resolved conflicts caused by 7b3a8cd8919399e0309c6343894a594e528d2f9a.

Change-Id: Ia90921a840a1dd0dad7d6617d123c59e79f803bf
(cherry picked from commit 3d53beb6f6e2c26dabc564b0905997f1b413a2e9)

manifests/profile/base/octavia.pp
@@ -45,7 +45,11 @@
 # [*oslomsg_rpc_use_ssl*]
 #   Enable ssl oslo messaging services
 #   Defaults to hiera('oslo_messaging_rpc_use_ssl', '0')
-
+#
+# [*enable_driver_agent*]
+#   Enable the driver agent
+#   Defaults to false
+#
 class tripleo::profile::base::octavia (
   $step                 = Integer(hiera('step')),
   $oslomsg_rpc_proto    = hiera('oslo_messaging_rpc_scheme', 'rabbit'),
@@ -54,6 +58,7 @@ class tripleo::profile::base::octavia (
   $oslomsg_rpc_port     = hiera('oslo_messaging_rpc_port', '5672'),
   $oslomsg_rpc_username = hiera('oslo_messaging_rpc_user_name', 'guest'),
   $oslomsg_rpc_use_ssl  = hiera('oslo_messaging_rpc_use_ssl', '0'),
+  $enable_driver_agent  = false
 ) {
   if $step >= 3 {
     $oslomsg_rpc_use_ssl_real = sprintf('%s', bool2num(str2bool($oslomsg_rpc_use_ssl)))
@@ -70,5 +75,9 @@ class tripleo::profile::base::octavia (
     include octavia::config
     include octavia::logging
     include octavia::service_auth
+
+    if $enable_driver_agent {
+      include octavia::driver_agent
+    }
   }
 }

spec/classes/tripleo_profile_base_octavia_spec.rb
@@ -34,6 +34,9 @@ describe 'tripleo::profile::base::octavia' do
       it 'should not do anything' do
         is_expected.to_not contain_class('octavia')
         is_expected.to_not contain_class('octavia::config')
+        is_expected.to_not contain_class('octavia::logging')
+        is_expected.to_not contain_class('octavia::service_auth')
+        is_expected.to_not contain_class('octavia::driver_agent')
       end
     end
 
@@ -47,12 +50,18 @@ describe 'tripleo::profile::base::octavia' do
           :default_transport_url => 'rabbit://guest:password@some.server.com:5672/?ssl=0'
         )
         is_expected.to contain_class('octavia::config')
+        is_expected.to contain_class('octavia::logging')
+        is_expected.to contain_class('octavia::service_auth')
+        is_expected.to_not contain_class('octavia::driver_agent')
       end
     end
 
     context 'with multiple hosts' do
       before do
-        params.merge!({ :oslomsg_rpc_hosts => ['some.server.com', 'someother.server.com'] })
+        params.merge!({
+          :step              => 3,
+          :oslomsg_rpc_hosts => ['some.server.com', 'someother.server.com']
+        })
       end
 
       it 'should construct a multihost URL' do
@@ -64,7 +73,10 @@ describe 'tripleo::profile::base::octavia' do
 
     context 'with username provided' do
       before do
-        params.merge!({ :oslomsg_rpc_username => 'bunny' })
+        params.merge!({
+            :step                 => 3,
+            :oslomsg_rpc_username => 'bunny'
+        })
       end
 
       it 'should construct URL with username' do
@@ -76,11 +88,11 @@ describe 'tripleo::profile::base::octavia' do
 
     context 'with username and password provided' do
       before do
-        params.merge!(
-          { :oslomsg_rpc_username  => 'bunny',
-            :oslomsg_rpc_password  => 'carrot'
-          }
-        )
+        params.merge!({
+          :step                  => 3,
+          :oslomsg_rpc_username  => 'bunny',
+          :oslomsg_rpc_password  => 'carrot'
+        })
       end
 
       it 'should construct URL with username and password' do
@@ -92,12 +104,12 @@ describe 'tripleo::profile::base::octavia' do
 
     context 'with multiple hosts and user info provided' do
       before do
-        params.merge!(
-          { :oslomsg_rpc_hosts     => ['some.server.com', 'someother.server.com'],
-            :oslomsg_rpc_username  => 'bunny',
-            :oslomsg_rpc_password  => 'carrot'
-          }
-        )
+        params.merge!({
+          :step                  => 3,
+          :oslomsg_rpc_hosts     => ['some.server.com', 'someother.server.com'],
+          :oslomsg_rpc_username  => 'bunny',
+          :oslomsg_rpc_password  => 'carrot'
+        })
       end
 
       it 'should distributed user info across hosts URL' do
@@ -106,6 +118,27 @@ describe 'tripleo::profile::base::octavia' do
         )
       end
     end
+
+    context 'with driver agent enabled' do
+      before do
+        params.merge!({
+          :step                => 3,
+          :enable_driver_agent => true
+        })
+      end
+
+      it 'should provide basic initialization' do
+        is_expected.to contain_class('octavia').with(
+          :default_transport_url => 'rabbit://guest:password@some.server.com:5672/?ssl=0'
+        )
+        is_expected.to contain_class('octavia::config')
+        is_expected.to contain_class('octavia::db')
+        is_expected.to contain_class('octavia::logging')
+        is_expected.to contain_class('octavia::service_auth')
+        is_expected.to contain_class('octavia::driver_agent')
+      end
+    end
+
   end
 
   on_supported_os.each do |os, facts|